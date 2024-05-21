0511f8ae8bf458d03a3b1457ac0849c4723e3f9e
alyan111 <alyan.mobipixels@gmail.com>
2024-05-21 14:57:48 +0500

    Reverting changes

--- Code/application/views/setting-forms/email-templates.php
+++ Code/application/views/setting-forms/email-templates.php
@@ -27,13 +27,32 @@
 
               <option value="<?= base_url('settings/email-templates/client_new_task') ?>" <?= ($this->uri->segment(3) == 'client_new_task') ? "selected" : "" ?>> <?= $this->lang->line('client_new_task') ? htmlspecialchars($this->lang->line('client_new_task')) : 'Client New task' ?> </option>
 
-              <!--<option value="<?= base_url('settings/email-templates/new_meeting') ?>" <?= ($this->uri->segment(3) == 'new_meeting') ? "selected" : "" ?>> <?= $this->lang->line('new_meeting') ? htmlspecialchars($this->lang->line('new_meeting')) : 'New meeting' ?> </option>-->
+              <!-- <option value="<?= base_url('settings/email-templates/new_meeting') ?>" <?= ($this->uri->segment(3) == 'new_meeting') ? "selected" : "" ?>> <?= $this->lang->line('new_meeting') ? htmlspecialchars($this->lang->line('new_meeting')) : 'New meeting' ?> </option> -->
 
               <option value="<?= base_url('settings/email-templates/new_invoice') ?>" <?= ($this->uri->segment(3) == 'new_invoice') ? "selected" : "" ?>> <?= $this->lang->line('new_invoice') ? htmlspecialchars($this->lang->line('new_invoice')) : 'New invoice' ?> </option>
 
               <option value="<?= base_url('settings/email-templates/new_estimate') ?>" <?= ($this->uri->segment(3) == 'new_estimate') ? "selected" : "" ?>> <?= $this->lang->line('new_estimate') ? htmlspecialchars($this->lang->line('new_estimate')) : 'New estimate' ?> </option>
 
               <option value="<?= base_url('settings/email-templates/front_enquiry_form') ?>" <?= ($this->uri->segment(3) == 'front_enquiry_form') ? "selected" : "" ?>> <?= $this->lang->line('frontend_enquiry_form') ? htmlspecialchars($this->lang->line('frontend_enquiry_form')) : "Frontend enquiry form" ?> </option>
+             
+             
+              <!-- ------ New Email templates  ------- -->
+              <option value="<?= base_url('settings/email-templates/new_user_registration_2') ?>" <?= ($this->uri->segment(3) == 'new_user_registration_2') ? "selected" : "" ?>> <?= $this->lang->line('new_user_registration_2') ? htmlspecialchars($this->lang->line('new_user_registration_2')) : "New User Registration template 2" ?> </option>
+              <option value="<?= base_url('settings/email-templates/forgot_password_2') ?>" <?= ($this->uri->segment(3) == 'forgot_password_2') ? "selected" : "" ?>> <?= $this->lang->line('forgot_password_2') ? htmlspecialchars($this->lang->line('forgot_password_2')) : 'Forgot password template 2' ?> </option>
+              <option value="<?= base_url('settings/email-templates/email_verification_2') ?>" <?= ($this->uri->segment(3) == 'email_verification_2') ? "selected" : "" ?>> <?= $this->lang->line('email_verification_2') ? htmlspecialchars($this->lang->line('email_verification_2')) : 'Email verification template 2' ?> </option>
+              <option value="<?= base_url('settings/email-templates/new_project_2') ?>" <?= ($this->uri->segment(3) == 'new_project_2') ? "selected" : "" ?>> <?= $this->lang->line('new_project_2') ? htmlspecialchars($this->lang->line('new_project_2')) : 'New Project template 2' ?> </option>
+              <option value="<?= base_url('settings/email-templates/new_task_2') ?>" <?= ($this->uri->segment(3) == 'new_task_2') ? "selected" : "" ?>> <?= $this->lang->line('new_task_2') ? htmlspecialchars($this->lang->line('new_task_2')) : 'New task 2' ?> </option>
+             <!-- <option value="<?= base_url('settings/email-templates/new_meeting_2') ?>" <?= ($this->uri->segment(3) == 'new_meeting_2') ? "selected" : "" ?>> <?= $this->lang->line('new_meeting_2') ? htmlspecialchars($this->lang->line('new_meeting_2')) : 'New meeting template 2' ?> </option> -->
+             <option value="<?= base_url('settings/email-templates/new_invoice_2') ?>" <?= ($this->uri->segment(3) == 'new_invoice_2') ? "selected" : "" ?>> <?= $this->lang->line('new_invoice_2') ? htmlspecialchars($this->lang->line('new_invoice_2')) : 'New invoice template 2' ?> </option>
+             <option value="<?= base_url('settings/email-templates/new_estimate_2') ?>" <?= ($this->uri->segment(3) == 'new_estimate_2') ? "selected" : "" ?>> <?= $this->lang->line('new_estimate_2') ? htmlspecialchars($this->lang->line('new_estimate_2')) : 'New estimate template 2' ?> </option>
+             <option value="<?= base_url('settings/email-templates/front_enquiry_form_2') ?>" <?= ($this->uri->segment(3) == 'front_enquiry_form_2') ? "selected" : "" ?>> <?= $this->lang->line('frontend_enquiry_form_2') ? htmlspecialchars($this->lang->line('frontend_enquiry_form_2')) : "Frontend enquiry form template 2" ?> </option>
+             <option value="<?= base_url('settings/email-templates/leave_request_2') ?>" <?= ($this->uri->segment(3) == 'leave_request_2') ? "selected" : "" ?>> <?= $this->lang->line('leave_request_2') ? htmlspecialchars($this->lang->line('leave_request_2')) : 'Leave Request Received template 2' ?> </option>
+             <option value="<?= base_url('settings/email-templates/biometric_2') ?>" <?= ($this->uri->segment(3) == 'biometric_2') ? "selected" : "" ?>> <?= $this->lang->line('biometric_2') ? htmlspecialchars($this->lang->line('biometric_2')) : 'Attendance 2' ?> </option>
+             <option value="<?= base_url('settings/email-templates/leave_accept_2') ?>" <?= ($this->uri->segment(3) == 'leave_accept_2') ? "selected" : "" ?>> <?= $this->lang->line('leave_accept_2') ? htmlspecialchars($this->lang->line('leave_accept_2')) : 'Leave Request Accepted 2' ?> </option>
+             <option value="<?= base_url('settings/email-templates/client_new_project_2') ?>" <?= ($this->uri->segment(3) == 'client_new_project_2') ? "selected" : "" ?>> <?= $this->lang->line('client_new_project_2') ? htmlspecialchars($this->lang->line('client_new_project_2')) : 'Client New project template 2' ?> </option>
+             <option value="<?= base_url('settings/email-templates/client_new_task_2') ?>" <?= ($this->uri->segment(3) == 'client_new_task_2') ? "selected" : "" ?>> <?= $this->lang->line('client_new_task_2') ? htmlspecialchars($this->lang->line('client_new_task_2')) : 'Client New task template 2' ?> </option>
+
+
 
             </select>
           </div>
@@ -45,6 +64,7 @@
           <div class="form-group col-md-12">
             <label class="col-form-label"><?= $this->lang->line('email_message') ? $this->lang->line('email_message') : 'Email Message' ?></label>
             <textarea type="text" name="message" class="form-control"><?= $template ? $template[0]['message'] : '' ?></textarea>
+
           </div>
 
           <div class="form-group col-md-12">
