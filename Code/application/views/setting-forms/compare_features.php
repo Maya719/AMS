ad8d36aab35e30949d5d8a98abc3577d0b875895
MNabeelAltaf <nabeelaltaf@outlook.com>
2024-05-22 17:46:09 +0500

    features_comparision

--- Code/application/views/setting-forms/compare_features.php
+++ Code/application/views/setting-forms/compare_features.php
@@ -49,7 +49,7 @@
                 }
             }
 
-            //  echo json_encode($featureNames);
+            // echo json_encode($featureNames);
 
 
 
@@ -83,25 +83,74 @@
             // ];
 
 
+            $ams_modules = ['attendance_management_system','attendance', 'leaves', 'leaves_types', 'leave_hierarchy', 'biometric_missing', 'shifts', 'biometric_machine', 'holidays'];
+            $pms_modules = ['project_management_system','projects', 'tasks', 'team_members', 'departments'];
+            $other_modules = ['user_roles', 'clients', 'todo', 'notice_board', 'notes', 'chat', 'user_permissions', 'notifications', 'reports', 'support'];
 
 
-            foreach ($featureNames as $feature => $displayName) {
-                if ($feature == 'project_management_system') {
-                    echo '<tr><td  colspan="' . (count($plans) + 1) . '"> <h3>' . htmlspecialchars($displayName) . '</h3></td></tr>';
-                } elseif ($feature == 'attendance_management_system') {
-                    echo '<tr><td colspan="' . (count($plans) + 1) . '"> <h3>' . htmlspecialchars($displayName) . '</h3></td></tr>';
-                } elseif($feature!='select_all') {
+
+         
+            $order = array_merge($other_modules, $pms_modules, $ams_modules);
+
+            foreach ($order as $module) {
+                if (isset($featureNames[$module])) {
+                    if ($module == 'project_management_system' || $module == 'attendance_management_system') {
+                        echo '<tr><td colspan="' . (count($plans) + 1) . '"><h3>' . htmlspecialchars($featureNames[$module]) . '</h3></td></tr>';
+                    } else {
+                        echo '<tr>';
+                        echo '<td>' . htmlspecialchars($featureNames[$module]) . '</td>';
+
+                        foreach ($plans as $plan) {
+                            $abs = json_decode($plan['modules'], true);
+                            $featureValue = isset($abs[$module]) ? ($abs[$module] == 1 ? '<i class="fas fa-check-circle text-success fs-4"></i>' : '<i class="fa-solid fa-circle-xmark text-danger fs-4"></i>') : '';
+                            echo '<td>' . $featureValue . '</td>';
+                        }
+                        echo '</tr>';
+                    }
+                 
+                    unset($featureNames[$module]);
+                }
+            }
+
+   
+            foreach ($featureNames as $module => $displayName) {
+                if ($module != 'select_all') {
                     echo '<tr>';
                     echo '<td>' . htmlspecialchars($displayName) . '</td>';
 
                     foreach ($plans as $plan) {
                         $abs = json_decode($plan['modules'], true);
-                        $featureValue = isset($abs[$feature]) ? ($abs[$feature] == 1 ? '<i class="fas fa-check-circle text-success fs-4"></i>' : '<i class="fa-solid fa-circle-xmark text-danger fs-4"></i>') : '';
+                        $featureValue = isset($abs[$module]) ? ($abs[$module] == 1 ? '<i class="fas fa-check-circle text-success fs-4"></i>' : '<i class="fa-solid fa-circle-xmark text-danger fs-4"></i>') : '';
                         echo '<td>' . $featureValue . '</td>';
                     }
                     echo '</tr>';
                 }
             }
+
+
+
+
+
+
+            // ===========================
+
+            // foreach ($featureNames as $feature => $displayName) {
+            //     if ($feature == 'project_management_system') {
+            //         echo '<tr><td  colspan="' . (count($plans) + 1) . '"> <h3>' . htmlspecialchars($displayName) . '</h3></td></tr>';
+            //     } elseif ($feature == 'attendance_management_system') {
+            //         echo '<tr><td colspan="' . (count($plans) + 1) . '"> <h3>' . htmlspecialchars($displayName) . '</h3></td></tr>';
+            //     } elseif($feature!='select_all') {
+            //         echo '<tr>';
+            //         echo '<td>' . htmlspecialchars($displayName) . '</td>';
+
+            //         foreach ($plans as $plan) {
+            //             $abs = json_decode($plan['modules'], true);
+            //             $featureValue = isset($abs[$feature]) ? ($abs[$feature] == 1 ? '<i class="fas fa-check-circle text-success fs-4"></i>' : '<i class="fa-solid fa-circle-xmark text-danger fs-4"></i>') : '';
+            //             echo '<td>' . $featureValue . '</td>';
+            //         }
+            //         echo '</tr>';
+            //     }
+            // }
             ?>
 
         </tbody>
